12525203cf79e0d9f2bd55fa3d6918e37fa58cbd
Vitalii Kharchenko <sir.vetal98@gmail.com>
2024-01-29 15:13:08 +0200

    Create DbContext for Notes

---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+
+namespace Notes.Persistence;
+
+public class NotesDbContext(DbContextOptions<NotesDbContext> options) : DbContext(options), INotesDbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new NoteConfiguration());
+        base.OnModelCreating(builder);
+    }
+}
