c34648ab63c2c19703c485e3f4b75763a6172b56
Vitalii Kharchenko <sir.vetal98@gmail.com>
2024-02-01 21:15:37 +0200

    Configure WebAPI

--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,6 +1,48 @@
+using System.Reflection;
+using Notes.Application;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var corsPolitics = "AllowAll";
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    cfg.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+builder.Services.AddApplication();
+builder.Services.AddPersistence(configuration);
+builder.Services.AddControllers();
+builder.Services.AddCors(
+    opt =>
+        opt.AddPolicy(corsPolitics, policy =>
+        {
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.AllowAnyOrigin();
+        }));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var ctx = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(ctx);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
 
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors(corsPolitics);
+app.MapControllers();
 app.Run();
